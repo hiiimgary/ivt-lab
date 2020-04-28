674bb8e9d3445c2b6373bffb6f4a047dcfb284f6
Gergő Pattantyus <pattantyus.gerg@gmail.com>
2020-04-28 18:54:38 +0200

    random moved

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -9,6 +9,7 @@ import java.util.Random;
 */
 public class TorpedoStore {
 
+  Random generator = new Random();
   // rate of failing to fire torpedos [0.0, 1.0]
   private double FAILURE_RATE = 0.0; //NOSONAR
 
@@ -27,7 +28,7 @@ public class TorpedoStore {
       }
     }
   }
-  Random generator = new Random();
+  
   public boolean fire(int numberOfTorpedos){
     if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
       throw new IllegalArgumentException("numberOfTorpedos");
